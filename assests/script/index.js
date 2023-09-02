abee7e01e1d81ee5f58649d363af149b26c02d90
cristobalwalters12 <81272662+cristobalwalters12@users.noreply.github.com>
2023-09-01 23:03:21 -0400

    desafio completo

--- assests/script/index.js
+++ assests/script/index.js
@@ -2,23 +2,43 @@ const apiDolar = "https://mindicador.cl/api/dolar";
 const apiEuro = "https://mindicador.cl/api/euro";
 const resultado = document.getElementById("resultado");
 const boton = document.getElementById("convertir");
+let chartDolar; 
+let chartEuro; 
+
+function sortByDateAscending(data) {
+  return data.sort((a, b) => new Date(a.fecha) - new Date(b.fecha));
+}
 
 async function getCurrencies() {
   try {
     const resDolar = await fetch(apiDolar);
     const dataDolar = await resDolar.json();
     const valorDolar = dataDolar.serie[0].valor;
+    const HistorialDolar = sortByDateAscending(dataDolar.serie.slice(0, 10));
+
     const resEuro = await fetch(apiEuro);
     const dataEuro = await resEuro.json();
     const valorEuro = dataEuro.serie[0].valor;
+    const HistorialEuro = sortByDateAscending(dataEuro.serie.slice(0, 10));
+
     return {
       dolar: valorDolar,
       euro: valorEuro,
+      HistorialEuro,
+      HistorialDolar
     };
   } catch (error) {
-    console.error("hubo un error: ", error);
+    console.error("Hubo un error: ", error);
   }
 }
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+  const year = date.getFullYear().toString().slice(2); 
+  return `${day}/${month}/${year}`;
+}
+
 boton.addEventListener("click", async function () {
   const valores = await getCurrencies();
   const valorDolar = valores.dolar;
@@ -30,47 +50,48 @@ boton.addEventListener("click", async function () {
   if (moneda === "dolar") {
     let valorTotalDolar = numero * valorDolar;
     resultado.innerText = "Resultado: $" + valorTotalDolar;
+    renderGrafica(valores.HistorialDolar, null);
   } else if (moneda === "euro") {
     let valorTotalEuro = numero * valorEuro;
     resultado.innerText = "Resultado: $" + valorTotalEuro;
+    renderGrafica(null, valores.HistorialEuro);
   }
 });
-async function getMonedas() {
-  const endpoint = "https://api.gael.cloud/general/public/monedas";
-  const res = await fetch(endpoint);
-  const monedas = await res.json();
-  return monedas;
-}
-function prepararConfiguracionParaLaGrafica(monedas) {
-  // Creamos las variables necesarias para el objeto de configuración
-  const tipoDeGrafica = "line";
-  const nombresDeLasMonedas = monedas.map((moneda) => moneda.Codigo);
-  const titulo = "Monedas";
-  const colorDeLinea = "red";
-  const valores = monedas.map((moneda) => {
-    const valor = moneda.Valor.replace(",", ".");
-    return Number(valor);
-  });
-  // Creamos el objeto de configuración usando las variables anteriores
-  const config = {
-    type: tipoDeGrafica,
-    data: {
-      labels: nombresDeLasMonedas,
-      datasets: [
-        {
-          label: titulo,
-          backgroundColor: colorDeLinea,
-          data: valores,
-        },
-      ],
-    },
-  };
-  return config;
-}
-async function renderGrafica() {
-  const monedas = await getMonedas();
-  const config = prepararConfiguracionParaLaGrafica(monedas);
+
+function renderGrafica(historialDolar, historialEuro) {
+  if (chartDolar) {
+    chartDolar.destroy();
+  }
+  if (chartEuro) {
+    chartEuro.destroy();
+  }
+
   const chartDOM = document.getElementById("myChart");
-  new Chart(chartDOM, config);
+  const ctx = chartDOM.getContext("2d");
+
+  if (historialDolar) {
+    chartDolar = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: historialDolar.map((entry) => formatDate(entry.fecha)),
+        datasets: [{
+          label: 'Dólar',
+          data: historialDolar.map((entry) => entry.valor),
+          backgroundColor: 'red'
+        }]
+      }
+    });
+  } else if (historialEuro) {
+    chartEuro = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: historialEuro.map((entry) => formatDate(entry.fecha)), 
+        datasets: [{
+          label: 'Euro',
+          data: historialEuro.map((entry) => entry.valor),
+          backgroundColor: 'blue'
+        }]
+      }
+    });
+  }
 }
-renderGrafica();
